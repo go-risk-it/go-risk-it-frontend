b95344235638816fefe87f6ec4f8f5cb703ad003
giovannizotta <giovannizotta771@gmail.com>
2025-01-13 00:10:55 +0100

    disable TLS in local dev setup

--- vite.config.ts
+++ vite.config.ts
@@ -9,8 +9,9 @@ export default defineConfig(({mode}) => {
         define: {
             "process.env.REACT_APP_WS_URL": JSON.stringify(env.REACT_APP_WS_URL),
             "process.env.REACT_APP_API_URL": JSON.stringify(env.REACT_APP_API_URL),
-            "process.env.REACT_APP_SUPABASE_URL": JSON.stringify(env.REACT_APP_SUPABASE_URL,),
-            "process.env.REACT_APP_SUPABASE_ANON_KEY": JSON.stringify(env.REACT_APP_SUPABASE_ANON_KEY,),
+            "process.env.REACT_APP_SUPABASE_URL": JSON.stringify(env.REACT_APP_SUPABASE_URL),
+            "process.env.REACT_APP_SUPABASE_ANON_KEY": JSON.stringify(env.REACT_APP_SUPABASE_ANON_KEY),
+            "process.env.NODE_TLS_VERIFY": JSON.stringify(env.NODE_TLS_VERIFY),
         },
     };
 });
