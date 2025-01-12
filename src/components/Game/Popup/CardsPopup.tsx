8f0ad24ebc9034ce1226b644aad28459142e9227
giovannizotta <giovannizotta771@gmail.com>
2025-01-12 16:26:51 +0100

    fix build

--- src/components/Game/Popup/CardsPopup.tsx
+++ src/components/Game/Popup/CardsPopup.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react"
 import Dialog from "@mui/material/Dialog"
-import DialogTitle from "@mui/material/DialogTitle"
 import DialogContent from "@mui/material/DialogContent"
 import DialogActions from "@mui/material/DialogActions"
 import Button from "@mui/material/Button"
